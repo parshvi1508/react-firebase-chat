ab39f17bc98bac2e2f1c228006df5c5e6cd8c48c
Parshvi Jain <parshvijain1508@gmail.com>
2024-11-26 21:31:36 +0530

    Basic frontend setup

---
+++ react-firebase-chat/README.md
@@ -0,0 +1,2 @@
+# A realtime Chat Application 
+This web application has been built in react and uses firebase.
---
+++ react-firebase-chat/src/App.jsx
@@ -0,0 +1,8 @@
+const App = () => {
+  return (
+    <div className='container'>
+      App</div>
+  )
+}
+
+export default App
